147cde298ba036e6faa92ea8bf0720e5b4d04ee1
Mykeleganerz <eganajustin@gmail.com>
2026-06-04 08:58:20 +0800

    deleted unused index.ts

--- backend/src/modules/notifications/index.ts
+++
@@ -1,4 +0,0 @@
-export { NotificationsModule } from './notifications.module';
-export { NotificationsService } from './notifications.service';
-export { NotificationsController } from './notifications.controller';
-export { SendReminderDto } from './dto/send-reminder.dto';
