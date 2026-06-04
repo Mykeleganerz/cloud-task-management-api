c2fa1d7f87caf3aa5b4aa63bfff3b865c58c00e0
Mykeleganerz <eganajustin@gmail.com>
2026-06-04 08:58:20 +0800

    deleted unused index.ts

--- backend/src/modules/task-reminders/index.ts
+++
@@ -1,3 +0,0 @@
-export { TaskRemindersModule } from './task-reminders.module';
-export { TaskRemindersService } from './task-reminders.service';
-export { TaskRemindersProcessor } from './processors/task-reminders.processor';
--- backend/src/modules/tasks/index.ts
+++
@@ -1,5 +0,0 @@
-export { TasksModule } from './tasks.module';
-export { TasksService } from './tasks.service';
-export { TasksController } from './tasks.controller';
-export { CreateTaskDto } from './dto/create-task.dto';
-export { UpdateTaskDto } from './dto/update-task.dto';
